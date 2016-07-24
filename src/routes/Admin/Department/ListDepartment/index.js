eb1af75a4ca09259f3f9a90da457932f09fbce9d
kris <kritika_soni@cmu.ac.th>
2016-07-24 22:07:07 +0700

    fix bug

--- src/routes/Admin/Department/ListDepartment/index.js
+++ src/routes/Admin/Department/ListDepartment/index.js
@@ -22,27 +22,3 @@ export default (store) => ({
     }, 'listDepartments')
   }
 })
-import { injectReducer } from '../../../../store/reducers'
-
-export default (store) => ({
-  path: 'admin/departments',
-  /*  Async getComponent is only invoked when route matches   */
-  getComponent (nextState, cb) {
-    /*  Webpack - use 'require.ensure' to create a split point
-     and embed an async module loader (jsonp) when bundling   */
-    require.ensure([], (require) => {
-      /*  Webpack - use require callback to define
-       dependencies for bundling   */
-      const ListDepartment = require('./ListDepartment.component').default
-      const reducer = require('./ListDepartment.reducer').default
-
-      /*  Add the reducer to the store on key 'counter'  */
-      injectReducer(store, { key: 'listDepartments', reducer })
-
-      /*  Return getComponent   */
-      cb(null, ListDepartment)
-
-      /* Webpack named bundle   */
-    }, 'listDepartments')
-  }
-})
