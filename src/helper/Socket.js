25db487e1af1ec57e60bfc4cea5fd5a7557001c9
kris <kritika_soni@cmu.ac.th>
2016-10-03 19:37:07 +0700

    edit socket

--- src/helper/Socket.js
+++ src/helper/Socket.js
@@ -1,13 +1,16 @@
 import { BackendUrl } from 'Config'
 const socketIOClient = require('socket.io-client')
 const sailsIOClient = require('sails.io.js')
+const io = sailsIOClient(socketIOClient)
+io.sails.url = BackendUrl
+
 const store = require('store');
 
 function create(){
-  const io = sailsIOClient(socketIOClient)
-  io.sails.url = BackendUrl
-  io.sails.headers = {
-    Authorization: `Bearer ${store.get('token') || ''}`
+  if(!io.sails.headers){
+    io.sails.headers = {
+      Authorization: `Bearer ${store.get('token') || ''}`
+    }
   }
   return io.socket;
 }
